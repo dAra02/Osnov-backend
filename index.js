1ecf913ff88f936304af063ed7b89f8f014e4fd1
DEX <you@example.com>
2024-06-10 15:10:41 +0300

    add fn remove

---
+++ index.js
@@ -0,0 +1,35 @@
+const yargs = require("yargs");
+const { addNote, printNotes, removeNotes } = require("./notes.controller");
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    addNote(title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    printNotes();
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove note by id",
+  async handler({ id }) {
+    removeNotes(id);
+  },
+});
+
+yargs.parse();
